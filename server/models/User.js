998ba1b09b7db313719a70a0af67a9d0a3a69fd2
Tab-Y <tlyoumans@gmail.com>
2022-12-12 13:42:35 -0500

    base structure for routes, user model, user controller

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Please use a valid email."],        // validates email
+        },
+        questions: [{
+            type: Schema.Types.ObjectId,
+            ref: "question"
+        }],
+        answers: [{
+            type: Schema.Types.ObjectId,
+            ref: 'answer'
+        }],
+        karma: {
+            type: Number,
+            min: 0,
+        }
+    }
+)
+
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
---
+++ server/routes/api/users/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const {  } = require('../controllers') // find specific functions file needed for this part of routes
+
+// each CRUD route
+// POST
+// GET
+// PUT
+// DELETE
+
+module.exports = router;
