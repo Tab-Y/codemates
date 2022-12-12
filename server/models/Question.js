69d579775c48d039a870a855181310e35bb1fac2
Tab-Y <tlyoumans@gmail.com>
2022-12-12 14:09:55 -0500

    model update for questions

--- server/models/Question.js
+++ server/models/Question.js
@@ -1,16 +1,37 @@
 const { Schema, model } = require('mongoose');
+const { answerSchema } = require('./Answer')
 
 const questionSchema = new Schema(
     {
-
+        content: {
+            type: String,
+            required: true,
+            minlength: 5,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        answers: {
+            type: [answerSchema],
+        },
     },
     {                               // as shown in assignment 26
         toJSON: {
-            getters: true,
+            virtuals: true,
         },
         id: false,
     },
 )
+
+questionSchema.virtual('answerCount').get( () => {
+    return this.answers.length;
+});
 
 const Question = model('question', questionSchema)
 
