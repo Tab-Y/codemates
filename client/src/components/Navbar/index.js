c7130461f0f085212ddc89cc7bd4cdb5e8e3cf9e
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-12 11:42:54 -0500

    redone navbar index js file

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -5,24 +5,44 @@ function Nav(props) {
 
     return (
 
-        <header id="flex-row">
-
-    <div>
-
-        <nav className="menu">
-          <input type="radio" name="nav-item" id="m-home" defaultChecked /><label htmlFor="m-home">Home</label>
-          <input type="radio" name="nav-item" id="m-search" /><label htmlFor="m-search">Search</label>
-          <input type="radio" name="nav-item" id="m-notification" /><label htmlFor="m-notification">Notification</label>
-          <input type="radio" name="nav-item" id="m-favorites" /><label htmlFor="m-favorites">Favorites</label>
-          <input type="radio" name="nav-item" id="m-profile" /><label htmlFor="m-profile">Profile</label>
-          <input type="radio" name="nav-item" id="m-saves" /><label htmlFor="m-profile">Profile</label>
-          <div className="selector" />
-        </nav>
-
-        {/* partial */}
-      </div>
-      </header>
-    )
-  }
+            <header id="flex-row">
+    
+                {/* <h2>
+                    <a href=""></a>
+                </h2> */}
+    
+                <nav className="flex-row">
+    
+                <ul className="nav-row">
+    
+                    {tabs.map((tab) => (
+                        <li className="mx-2" key={tab}>
+    
+                            <a
+                                href={'#' + tab.toLowerCase()}
+                                onClick={() => props.handlePageChange(tab)}
+                                className={
+                                    props.currentPage === tab
+                                        ? "navActive"
+                                        : "mx-2"
+                                }
+    
+                            >
+                                {tab}
+    
+                            </a>
+    
+                        </li>
+    
+                    ))}
+    
+                </ul>
+    
+            </nav>
+    
+        </header>
+        
+        );
+    }
 
   export default Nav;
