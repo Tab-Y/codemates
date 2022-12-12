269196b8f817d7b94740f776d8480672b11222f8
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-12-12 11:20:25 -0500

    added components folder with in src folder in client folder. Also created Navbar file and added index js code for the file and in App.js file for imports

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Nav(props) {
+    const tabs = ['Home', 'Search', 'Notification', 'Favorites', 'Profile']
+
+    return (
+
+        <header id="flex-row">
+
+    <div>
+
+        <nav className="menu">
+          <input type="radio" name="nav-item" id="m-home" defaultChecked /><label htmlFor="m-home">Home</label>
+          <input type="radio" name="nav-item" id="m-search" /><label htmlFor="m-search">Search</label>
+          <input type="radio" name="nav-item" id="m-notification" /><label htmlFor="m-notification">Notification</label>
+          <input type="radio" name="nav-item" id="m-favorites" /><label htmlFor="m-favorites">Favorites</label>
+          <input type="radio" name="nav-item" id="m-profile" /><label htmlFor="m-profile">Profile</label>
+          <div className="selector" />
+        </nav>
+
+        {/* partial */}
+      </div>
+      </header>
+    )
+  }
+
+  export default Nav;
