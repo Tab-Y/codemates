c2543074beb111fb62b428d536b95a94dfe8f9f9
Tab-Y <tlyoumans@gmail.com>
2022-12-12 14:13:36 -0500

    model update from Answers

--- server/models/Answer.js
+++ server/models/Answer.js
@@ -1,15 +1,32 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 const answerSchema = new Schema(
     {
-
+        answerId: {
+            type: Schema.Types.ObjectId,
+            default: () => Types.ObjectId(),
+        },
+        answerContent: {
+            type: String,
+            required: true,
+            minlength: 5,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
     },
-    {                               // as shown in assignment 26
+    {                               
         toJSON: {
           getters: true,
         },
         id: false,
       },
-)
+);
 
 module.exports = answerSchema;
