9cf49c1c7812a5356c4217ae1b616f98dd2a9c3d
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-12-13 14:46:38 -0500

    a basic structure of new answer page

--- client/src/components/NewProblem/index.js
+++ client/src/components/NewProblem/index.js
@@ -1,21 +1,21 @@
 import React from "react"
- 
+
 const NewProblem = () => {
 
-    return (
-        <div>
-        <h2>Create New Problem</h2>
-        <div class="card border-success mb-3" style="max-width: 18rem;">
-  <div class="card-header bg-transparent border-success">Problem Title/Question</div>
-  <div class="card-body text-success">
-    <h5 class="card-title">Question:</h5>
-    <p class="card-text">The question description</p>
-  </div>
-  <div class="card-footer bg-transparent border-success">Bank Point reward: Must be at least 5 pts</div>
-</div>
-<button type="button" class="btn btn-primary">Create Problem</button>
+  return (
+    <div>
+      <h2>Create New Problem</h2>
+      <div class="card border-success mb-3" style="max-width: 18rem;">
+        <div class="card-header bg-transparent border-success">Problem Title/Question</div>
+        <div class="card-body text-success">
+          <h5 class="card-title">Question:</h5>
+          <p class="card-text">The question description</p>
+        </div>
+        <div class="card-footer bg-transparent border-success">Bank Point reward: Must be at least 5 pts</div>
+      </div>
+      <button type="button" class="btn btn-primary">Create Problem</button>
     </div>
-    );
+  );
 };
 
 export default NewProblem;
