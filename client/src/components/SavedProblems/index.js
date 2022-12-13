f51aa24f85e5e77fd7119fc927359b9bbf17d402
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-12-13 14:21:44 -0500

    basic structure of saved problems page

---
+++ client/src/components/SavedProblems/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const SavedProblems = () => {
+    
+    return (
+        <div>
+            <h2>Saved Problems</h2>
+            <ul class="list-group">
+  <li class="list-group-item">An item</li>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item">A fourth item</li>
+  <li class="list-group-item">And a fifth one</li>
+</ul>
+        </div>
+    )
+}
