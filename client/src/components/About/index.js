8bb45626bc89077b0d60d9c6015de9630e0832ae
Jessica Almand <jlalmand27@gmail.com>
2022-12-14 14:25:05 -0500

    Fixed up About to work with react

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Logo from '../images/logo.jpg';
+import CPic1 from '../images/CarouselPic1.jpg';
+import CPic2 from '../images/CarouselPic2.jpg';
+import CPic3 from '../images/CarouselPic3.jpg';
+import CPic4 from '../images/CarouselPic4.jpg';
 
 const About = () => {
     return (
         <div>
         <header>
-          <img src="./assets/images/logo.jpg" alt="Logo">
+          <img src={Logo} alt="Logo"></img>
           <div class="p-5 text-center" style="background-color: #c4fff9">
             <h1 class="mb-3">Meet our developers</h1>
             <h4 class="mb-3">We are UCF Bootcamp students learning Full-Stack Web Development. Check out our links below to view our previous work.</h4>
@@ -21,35 +25,35 @@ const About = () => {
               </div>
               <div class="carousel-inner carousel-fade">
                 <div class="carousel-item active">
-                  <img src="./assets/images/CarouselPic1.jpg" class="d-block w-100" alt="...">
-                  <div class="carousel-caption" style="background-color: #051923;">
+                  <img src={CPic1} class="d-block w-100" alt="CPic1"></img>
+                  <div class="carousel-caption" style={{backgroundColor: "#051923"}}>
                     <h1>Taber Youmans</h1>
-                    <a style="color: white" href="https://github.com/Tab-Y"><iconify-icon icon="ri:github-fill" width="100" height="100"></iconify-icon></a>
-                      <a style="color: white" href="https://www.linkedin.com/in/taber-youmans-500587131/"><iconify-icon icon="mdi:linkedin" width="100" height="100"></iconify-icon></a>
+                    <a style={{color: "white"}} href="https://github.com/Tab-Y"><iconify-icon icon="ri:github-fill" width="100" height="100"></iconify-icon></a>
+                      <a style={{color: "white"}} href="https://www.linkedin.com/in/taber-youmans-500587131/"><iconify-icon icon="mdi:linkedin" width="100" height="100"></iconify-icon></a>
                   </div>
                 </div>
                 <div class="carousel-item">
-                  <img src="./assets/images/CarouselPic2.jpg" class="d-block w-100" alt="...">
-                  <div class="carousel-caption" style="background-color: #051923;">
+                  <img src={CPic2} class="d-block w-100" alt="CPic2"></img>
+                  <div class="carousel-caption" style={{backgroundColor: "#051923"}}>
                     <h1>Maria Constanza</h1>
-                    <a style="color: white" href="https://github.com/MariaYConstanza"><iconify-icon icon="ri:github-fill" width="100" height="100"></iconify-icon></a>
-                      <a style="color: white" href="https://www.linkedin.com/in/maria-constanza-09a1011b3/"><iconify-icon icon="mdi:linkedin" width="100" height="100"></iconify-icon></a>
+                    <a style={{color: "white"}} href="https://github.com/MariaYConstanza"><iconify-icon icon="ri:github-fill" width="100" height="100"></iconify-icon></a>
+                      <a style={{color: "white"}} href="https://www.linkedin.com/in/maria-constanza-09a1011b3/"><iconify-icon icon="mdi:linkedin" width="100" height="100"></iconify-icon></a>
                   </div>
                 </div>
                 <div class="carousel-item">
-                  <img src="./assets/images/CarouselPic3.jpg" class="d-block w-100" alt="...">
-                  <div class="carousel-caption" style="background-color: #051923;">
+                  <img src={CPic3} class="d-block w-100" alt="CPic3"></img>
+                  <div class="carousel-caption" style={{backgroundColor: "#051923"}}>
                     <h1>Zechariah Naitram</h1>
-                    <a style="color: white" href="https://github.com/Zechariahmn"><iconify-icon icon="ri:github-fill" width="100" height="100"></iconify-icon></a>
-                      <a style="color: white" href="https://www.linkedin.com/in/zechariah-naitram-5179aa250/"><iconify-icon icon="mdi:linkedin" width="100" height="100"></iconify-icon></a>
+                    <a style={{color: "white"}} href="https://github.com/Zechariahmn"><iconify-icon icon="ri:github-fill" width="100" height="100"></iconify-icon></a>
+                      <a style={{color: "white"}} href="https://www.linkedin.com/in/zechariah-naitram-5179aa250/"><iconify-icon icon="mdi:linkedin" width="100" height="100"></iconify-icon></a>
                   </div>
                 </div>
                 <div class="carousel-item">
-                  <img src="./assets/images/CarouselPic4.jpg" class="d-block w-100" alt="...">
-                  <div class="carousel-caption" style="background-color: #051923;">
+                  <img src={CPic4} class="d-block w-100" alt="CPic4"></img>
+                  <div class="carousel-caption" style={{backgroundColor: "#051923"}}>
                     <h1>Jessica Almand</h1>
-                    <a style="color: white" href="https://github.com/jalmand2"><iconify-icon icon="ri:github-fill" width="100" height="100"></iconify-icon></a>
-                      <a style="color: white" href="https://www.linkedin.com/in/jessicaalmand/"><iconify-icon icon="mdi:linkedin" width="100" height="100"></iconify-icon></a>
+                    <a style={{color: "white"}} href="https://github.com/jalmand2"><iconify-icon icon="ri:github-fill" width="100" height="100"></iconify-icon></a>
+                      <a style={{color: "white"}} href="https://www.linkedin.com/in/jessicaalmand/"><iconify-icon icon="mdi:linkedin" width="100" height="100"></iconify-icon></a>
                   </div>
                 </div>
               </div>
