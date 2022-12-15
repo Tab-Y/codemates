daded6dfb6ba993e06c79eb6bfcad885d9377e82
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-12-15 16:27:02 -0500

    final styling template for save problem page

--- client/src/components/SavedProblems/index.js
+++ client/src/components/SavedProblems/index.js
@@ -1,16 +1,28 @@
 import React from "react"
 
+const styles = {
+    h2: {
+        color: '#c4fff9',
+        marginLeft: '8cm',
+        marginTop: '8cm',
+    },
+    list: {
+        backgroundColor: '#c4fff9',
+        color: '#051923',
+    }
+}
+
 const SavedProblems = () => {
 
     return (
         <div>
-            <h2>Saved Problems</h2>
+            <h2 style={styles.h2}>Saved Problems</h2>
             <ul class="list-group problem-list">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-                <li class="list-group-item">A fourth item</li>
-                <li class="list-group-item">And a fifth one</li>
+                <li class="list-group-item" style={styles.list}>An item</li>
+                <li class="list-group-item" style={styles.list}>A second item</li>
+                <li class="list-group-item" style={styles.list}>A third item</li>
+                <li class="list-group-item" style={styles.list}>A fourth item</li>
+                <li class="list-group-item" style={styles.list}>And a fifth one</li>
             </ul>
         </div>
     );
