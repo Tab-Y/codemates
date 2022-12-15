5b89debc7e7f41b95966436ec758872d9fb82705
Tab-Y <tlyoumans@gmail.com>
2022-12-15 10:43:21 -0500

    added login auth function

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -4,10 +4,48 @@ const path = require('path');
 router.get('/', async (req, res) => {
     try {
        
-        res.sendFile(path.join(__dirname, '../../client/public/index.html'));
+        
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+
+// create
+router.post('/', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+// read
+router.get('/', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+// update
+router.put('/', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+// delete
+router.delete('/', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
 
 module.exports = router;
