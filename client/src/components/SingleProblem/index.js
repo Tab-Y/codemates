af3538e8f9eb76bc4b195bbca145bd15231650b0
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-12-16 11:06:12 -0500

    final styling template for single problem page

--- client/src/components/SingleProblem/index.js
+++ client/src/components/SingleProblem/index.js
@@ -1,32 +1,42 @@
 import React from "react"
 
+const styles = {
+    h2: {
+        color: '#c4fff9',
+    },
+    card: {
+        backgroundColor: '#c4fff9',
+        color: '#051923',
+    }
+}
+
 const SingleProblem = () => {
 
     return (
         <div>
             <section>
-                <h2 class="single">Single Problem Title/Question</h2>
+                <h2 class="single" style={styles.h2}>Single Problem Title/Question</h2>
                 <div class="row row-cols-1 row-cols-md-3 g-4 single-box">
                     <div class="col">
                         <div class="card h-100">
-                            <div class="card-body">
+                            <div class="card-body" style={styles.card}>
                                 <p class="card-text">Problem description</p>
                             </div>
                             <div class="card-footer">
-                                <small class="text-muted">Created by username: date & time</small>
+                                <small class="text-muted" style={styles.card}>Created by username: date & time</small>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
             <section>
-                <h2 class="answers">Answers</h2>
+                <h2 class="answers" style={styles.h2}>Answers</h2>
                 <ul class="list-group answer-box">
-                    <li class="list-group-item">An item</li>
-                    <li class="list-group-item">A second item</li>
-                    <li class="list-group-item">A third item</li>
-                    <li class="list-group-item">A fourth item</li>
-                    <li class="list-group-item">And a fifth one</li>
+                    <li class="list-group-item" style={styles.card}>An item</li>
+                    <li class="list-group-item" style={styles.card}>A second item</li>
+                    <li class="list-group-item" style={styles.card}>A third item</li>
+                    <li class="list-group-item" style={styles.card}>A fourth item</li>
+                    <li class="list-group-item" style={styles.card}>And a fifth one</li>
                 </ul>
             </section>
         </div>
